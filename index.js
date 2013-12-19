ced5339aab02cfede57808dc9b0a79c497257050
TJ Holowaychuk <tj@vision-media.ca>
2013-12-18 21:28:36 -0800

    app function

--- index.js
+++ index.js
@@ -4,4 +4,25 @@
  */
 
 var koa = require('koa');
-var app = module.exports = koa();
+
+/**
+ * Expose `api()`.
+ */
+
+module.exports = api;
+
+/**
+ * Initialize an app with the given `opts`.
+ *
+ * @param {Object} opts
+ * @return {Application}
+ * @api public
+ */
+
+function api(opts) {
+  opts = opts || {};
+
+  var app = koa();
+
+  return app;
+}
