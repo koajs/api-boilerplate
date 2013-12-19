efd147b55ff6658dc3870c7a16c37a3b381f77d1
TJ Holowaychuk <tj@vision-media.ca>
2013-12-18 21:57:24 -0800

    better example

--- api/stats/index.js
+++ api/stats/index.js
@@ -1,8 +1,28 @@
 
-exports.get = function *(){
-  this.body = {
-    requests: 100000,
-    average_duration: 52,
-    uptime: 123123132
-  }
+/**
+ * This file illustrates how you may map
+ * single routes using config.json instead
+ * of resoure based routing.
+ */
+
+var stats = {
+  requests: 100000,
+  average_duration: 52,
+  uptime: 123123132
+};
+
+/**
+ * GET all stats.
+ */
+
+exports.all = function *(){
+  this.body = stats;
+};
+
+/**
+ * GET a single stat.
+ */
+
+exports.all = function *(name){
+  this.body = stats[name];
 };
